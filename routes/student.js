7dec95d39025e7d755d8c3bc9a014c931674d71f
Ronit <70330163+asawaronit60@users.noreply.github.com>
2022-06-15 12:31:46 +0530

    disabled added

--- routes/student.js
+++ routes/student.js
@@ -7,5 +7,6 @@ router.post('/createStudent',studentController.createStudent)
 router.patch('/updateStudent/:id',studentController.updateStudent)
 router.delete('/deleteStudeent/:id',studentController.deleteStudent)
 router.delete('/deleteBulkStudents',studentController.bulkDelete)
+router.get('/disabledStudent',studentController.disabledStudents)
 
 module.exports = router
