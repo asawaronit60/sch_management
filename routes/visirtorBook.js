a5833a9ade2ee79bed370e14281a8b60c22bb169
Ronit <70330163+asawaronit60@users.noreply.github.com>
2022-06-16 20:30:52 +0530

    visitor  error 1

--- routes/visirtorBook.js
+++ routes/visirtorBook.js
@@ -4,7 +4,7 @@ const router  =express.Router()
 
 router.get('/getAllvisitorBook',visitorBookController.getAllVisitorsBook)
 router.post('/createVisitorBook',visitorBookController.createVisitorBook)
-router.patch('/updateVisitorBook:/id',visitorBookController.updateVisitorBook)
-router.delete('/deleteVisitorBook:/id',visitorBookController.deleteVisitorBook)
+router.patch('/updateVisitorBook/:id',visitorBookController.updateVisitorBook)
+router.delete('/deleteVisitorBook/:id',visitorBookController.deleteVisitorBook)
 
 module.exports  = router
