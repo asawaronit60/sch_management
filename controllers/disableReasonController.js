9b6924a686a4aa6f5a4dccabbb1d5a3f7500670a
Ronit <70330163+asawaronit60@users.noreply.github.com>
2022-06-20 18:14:59 +0530

    disable reason controoler

--- controllers/disableReasonController.js
+++ controllers/disableReasonController.js
@@ -1,74 +1,12 @@
 const DisableReason = require('../models/DisableReason')
+const ApiFactory = require('../utils/apiFactory')
 
-exports.getAllReasons = async(req,res)=>{
-  try {
-
-    let data = await  DisableReason.findAll()
-    
-    res.status(400).json({
-      status:'success',
-      data
-    })
-
-  } catch (err) {
-    res.status(400).json({
-      status:'fail',
-      message:err.message
-    })
-  }
-}
+exports.getAllReasons = ApiFactory.getAll(DisableReason)
 
 
-exports.createReason = async(req,res)=>{
-
-  try {
-
-    await DisableReason.create(req.body)
-
-    res.status(400).json({
-      status:'success',
-      message:'Reason created successfully!'
-    })
-  } catch (err) {
-    res.status(400).json({
-      status:'fail',
-      message:err.message
-    })
-  }
-}
+exports.createReason = ApiFactory.create(DisableReason)
 
 
-exports.deleteReason = async(req,res)=>{
-  try {
-    
-    await DisableReason.destroy({where:{id:req.params.id}})
-    res.status(400).json({
-      status:'success',
-      message:'Reason deleted successfully!'
-    })
-    
+exports.deleteReason = ApiFactory.delete(DisableReason)
 
-  } catch (err) {
-    res.status(400).json({
-      status:'fail',
-      message:err.message
-    })
-  }
-}
-
-exports.updateReason = async(req,res)=>{
-  try {
-    
-    await DisableReason.update(req.body,{where:{id:req.params.id}})
-    res.status(400).json({
-      status:'success',
-      message:'Reason Updated successfully!'
-    })
-    
-  } catch (err) {
-    res.status(400).json({
-      status:'fail',
-      message:err.message
-    })
-  }
-}
+exports.updateReason = ApiFactory.update(DisableReason)
