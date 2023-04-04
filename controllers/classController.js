6f84fb9c1db81c17331c8be2f3309664588e91a2
Ronit <70330163+asawaronit60@users.noreply.github.com>
2023-04-04 18:13:10 +0530

    class sec err

--- controllers/classController.js
+++ controllers/classController.js
@@ -4,51 +4,57 @@ const Section = require('../models/Section')
 
 const ApiFactory = require('../utils/apiFactory')
 
-exports.getAllClass = async(req,res,next)=>{
+exports.getAllClass = async (req, res, next) => {
 
   try {
 
     let results = []
 
     let classes = await Class.findAll({
-      attributes:['id']
+      attributes: ['id', 'class']
     })
 
-    for (const cl of classes){
+    for (const cl of classes) {
 
       let obj = {}
 
-      let sections = [] 
+      let sections = []
 
-      let class_id = cl.getDataValue('id')
+      // let class_id = cl.getDataValue('id')
 
-      let data = await classSection.findAll({where:{class_id},
-          include:[{
-            model:Class,
-            attributes:['class']
-          },{
-            model:Section,
-            attributes:['section']
-          }]
-        })
+      let data = await classSection.findAll({
+        where: { class_id: cl.getDataValue('id') },
+        include: [
+          {
+            model: Class,
+            attributes: ['id', 'class']
+          },
+          {
+            model: Section,
+            attributes: ['id', 'section']
+          }
+        ]
+      })
 
-        if(data.length>0)
-        obj.class = data[0].class.class
-        obj.id = class_id
-        data.forEach(el=>{
-          sections.push(el.section.section)
-        })
 
-        obj.section = sections
+      obj.class = cl.getDataValue('class')
 
-        results.push(obj)
+      obj.class_id = cl.getDataValue('id')
 
-      }//for 
+      data.forEach(el => {
+        sections.push({ id: el.section.id, section: el.section.section })
+      })
+
+      obj.sections = sections
+
+      results.push(obj)
+
+    }//for 
 
 
     res.status(200).json({
-      status:'success',
-      data:results
+      status: 'success',
+      data: results
     })
 
   } catch (err) {
@@ -60,74 +66,74 @@ exports.getAllClass = async(req,res,next)=>{
 
 
 
-exports.createClass = async(req,res)=>{
+exports.createClass = async (req, res) => {
 
   try {
-    
-    let { section_id , ...remaining } = req.body
+
+    let { section_id, ...remaining } = req.body
 
     let newClass = await Class.create(remaining)
 
-   for(const id of section_id){
+    for (const id of section_id) {
 
       let obj = {}
       obj.class_id = newClass.getDataValue('id')
       obj.section_id = id
 
       await classSection.create(obj)
-      
+
     }
 
-   res.status(200).json({
-    status:'success',
-    message:'Class created succesfully!',
- 
-   })
+    res.status(200).json({
+      status: 'success',
+      message: 'Class created succesfully!',
+
+    })
 
 
 
   } catch (err) {
     res.status(400).json({
-      status:'fail',
-      message:err.message
+      status: 'fail',
+      message: err.message
     })
   }
 
 
 }
 
-exports.getClassSections = async(req,res)=>{
+exports.getClassSections = async (req, res) => {
 
   try {
-    
+
     let sections = await classSection.findAll({
-      attributes:['id'],
-      where:{
-        class_id:req.params.class_id
+      attributes: ['id'],
+      where: {
+        class_id: req.params.class_id
       },
-      include:{
-        model:Section,
-        attributes:['id' , 'section']
+      include: {
+        model: Section,
+        attributes: ['id', 'section']
       }
     })
 
 
 
-    if(sections.length===0)
-    return res.status(404).json({
-      status:'fail',
-      message:'no sections found!'
-    })
+    if (sections.length === 0)
+      return res.status(404).json({
+        status: 'fail',
+        message: 'no sections found!'
+      })
 
     res.status(200).json({
-      status:'success',
-      data:sections
+      status: 'success',
+      data: sections
     })
 
   } catch (err) {
     res.status(400).json({
-      status:'fail',
-      message:err.message
+      status: 'fail',
+      message: err.message
     })
   }
 
