00d174822ff1e6d2d047bc5da7677a64b4e18087
Ronit <70330163+asawaronit60@users.noreply.github.com>
2023-04-26 17:26:41 +0530

    no er

--- controllers/marksGradeController.js
+++ controllers/marksGradeController.js
@@ -5,55 +5,55 @@ exports.getAllMarksGrade = async(req,res,next)=>{
 
   try {
 
-    // let examTypes = await examType.findAll({
-    //   attributes:['id','exam_type']
-    // })
-
-    // let results = []
-
-    // for(const examtype of examTypes){
-      
-    //   let obj = {}
-    //   obj.id = examtype.getDataValue('id')
-    //   obj.exam_type = examtype.getDataValue('exam_type')
-
-    //   let grade_name = [] 
-    //   let percent_upto = [] 
-    //   let percent_from = [] 
-    //   let grade_point = []
-
-    //   let marks_grades = await marksGrade.findAll({
-    //     where:{
-    //       exam_type_id:examtype.getDataValue('id') 
-    //     }
-    //   })
-
-    //   obj.grade = marks_grades
-
-    //   // grade_name = marks_grades.getDataValue('grade_name').map(gn=>gn)
-    //   // percent_upto = marks_grades.getDataValue('percent_upto').map(pu=>pu)
-    //   // percent_from = marks_grades.getDataValue('percent_from').map(pf=>pf)
-    //   // grade_point = marks_grades.getDataValue('grade_point').map(gp=>gp)
-
-    //   // obj.grade_name = grade_name
-    //   // obj.percent_upto = percent_upto
-    //   // obj.percent_from = percent_from
-    //   // obj.grade_point = grade_point
-
-    //   results.push(obj)
-
-    // }
-
-
-    let data =await marksGrade.findAll({
-      include:{
-        model:examType
-      }
+    let examTypes = await examType.findAll({
+      attributes:['id','exam_type']
     })
+
+    let results = []
+
+    for(const examtype of examTypes){
+      
+      let obj = {}
+      obj.id = examtype.getDataValue('id')
+      obj.exam_type = examtype.getDataValue('exam_type')
+
+      let grade_name = [] 
+      let percent_upto = [] 
+      let percent_from = [] 
+      let grade_point = []
+
+      let marks_grades = await marksGrade.findAll({
+        where:{
+          exam_type_id:examtype.getDataValue('id') 
+        }
+      })
+
+      obj.grade = marks_grades
+
+      // grade_name = marks_grades.getDataValue('grade_name').map(gn=>gn)
+      // percent_upto = marks_grades.getDataValue('percent_upto').map(pu=>pu)
+      // percent_from = marks_grades.getDataValue('percent_from').map(pf=>pf)
+      // grade_point = marks_grades.getDataValue('grade_point').map(gp=>gp)
+
+      // obj.grade_name = grade_name
+      // obj.percent_upto = percent_upto
+      // obj.percent_from = percent_from
+      // obj.grade_point = grade_point
+
+      results.push(obj)
+
+    }
+
+
+    // let data =await marksGrade.findAll({
+    //   include:{
+    //     model:examType
+    //   }
+    // })
 
     res.status(200).json({
       status:'success',
-      data
+      data:results
     })
 
 
