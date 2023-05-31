96a63ea656ac27e486983d3def47049cc4baa0eb
Ronit <70330163+asawaronit60@users.noreply.github.com>
2023-05-31 07:17:50 +0530

    new changes

--- routes/topics.js
+++ routes/topics.js
@@ -8,5 +8,10 @@ router.delete('/:id' , TopicController.deleteTopic)
 router.patch('/:id' , TopicController.updateTopic)
 
 router.get('/topicStatus/:class_id/:section_id/:subject_group_id/:subject_id',TopicController.getSubjectStatus)
+
+/**
+ * @body status
+ * @body topic_completion_date 
+ */
 router.patch('/topicStatus/:id',TopicController.updateTopicStatus)
 module.exports  = router
